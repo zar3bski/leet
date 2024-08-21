f5738332f1528f34ba3f2795fc79d4a76ef06798
zar3bski <zarebskidavid@gmail.com>
2024-08-21 19:38:22 +0200

    feat:less hard mapping in translation

--- src/translate.rs
+++ src/translate.rs
@@ -1,11 +1,15 @@
 use rand::seq::SliceRandom;
 
-use crate::mappings::MAPPING;
+use crate::mappings::{HARD_MAPPING, MAPPING};
 
 pub fn translate(message: &str) -> String {
     let mut translated = String::from("");
     for (i, c) in message.chars().enumerate() {
-        let candidates = MAPPING.get(&c);
+        let candidates = if i % 6 == 0 {
+            HARD_MAPPING.get(&c)
+        } else {
+            MAPPING.get(&c)
+        };
         if candidates.is_none() || i % 2 == 1 {
             translated.push(c);
         } else {
